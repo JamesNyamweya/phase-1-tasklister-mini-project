03c85ad8f9112bd0c30d42be476752b90daa4f70
James Nyamweya <nyamweyaj@gmail.com>
2025-03-17 05:25:37 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+    let taskList = document.getElementById("tasks");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let taskInput = document.getElementById("new-task-description");
+        let taskText = taskInput.value;
+
+        if (taskText !== "") {
+            let taskItem = document.createElement("li");
+            taskItem.textContent = taskText;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "X";
+            deleteButton.style.marginLeft = "10px";
+            deleteButton.addEventListener("click", () => {
+                taskList.removeChild(taskItem);
+            });
+
+            taskList.appendChild(taskItem);
+            taskList.appendChild(deleteButton);
+        }
+
+        taskInput.value = "";
+    });
 });
